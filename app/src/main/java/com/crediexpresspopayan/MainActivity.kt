354f9fea8d965679f5526c001df361924b2270ef
Jesus Alberto Mosquera Sandoval <jesus.ams123@gmail.com>
2023-05-11 18:27:01 -0500

    Init Commit

--- app/src/main/java/com/crediexpresspopayan/MainActivity.kt
+++ app/src/main/java/com/crediexpresspopayan/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CrediexpressPopayanTheme {
-        Greeting("Android Dev")
+        Greeting("Android Dev vd")
     }
 }
